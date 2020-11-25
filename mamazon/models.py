3abfbceb7e4896f90e84ec0f20f340bac42e5880
IgaKakeru <jolie_fille_aime@yahoo.co.jp>
2020-11-25 11:08:18 +0900

    lesson04

--- mamazon/models.py
+++ mamazon/models.py
@@ -6,4 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=150)
     price = models.IntegerField()
     description = models.TextField()
-    thumbnail = models.ImageField(upload_to='thumbnails/')
+    thumbnail = models.ImageField(upload_to='thumbnails/') #Pillowが必要
+
+    def __str__(self):
+        return self.name
